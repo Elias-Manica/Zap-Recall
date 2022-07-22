82ab53a71670d988859c01b007ea1db2703cf3d0
elias <eliasmanica@poli.ufrj.br>
2022-07-22 14:01:55 -0300

    ajustando css

--- src/components/Body.js
+++ src/components/Body.js
@@ -11,7 +11,7 @@ export default function Body() {
 
   return (
     <div className="allScreens">
-      <div>
+      <>
         {changeScreen ? (
           <>
             <WelcomeUser />
@@ -25,7 +25,7 @@ export default function Body() {
         ) : (
           <InicialDisplay />
         )}
-      </div>
+      </>
     </div>
   );
 }
