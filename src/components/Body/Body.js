19d1942c76b704ebac75d35007444e54c9adf4fd
elias <eliasmanica@poli.ufrj.br>
2022-07-25 17:56:40 -0300

    tirando o bug de embaralhar

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -9,6 +9,42 @@ import InicialDisplay from "../InicialDisplay/InicialDisplay";
 
 export default function Body() {
   const [changeScreen, setChangeScreen] = React.useState(true);
+  const [group, setGroup] = React.useState([
+    {
+      numb: 1,
+      question: "O que é JSX?",
+      answer: "Uma extensão de linguagem do JavaScript",
+    },
+    {
+      question: "O React é __",
+      answer: "uma biblioteca JavaScript para construção de interfaces",
+    },
+    {
+      question: "Componentes devem iniciar com __ ",
+      answer: "letra maiúscula",
+    },
+    {
+      question: "Podemos colocar __ dentro do JSX",
+      answer: "expressões",
+    },
+    {
+      question: "O ReactDOM nos ajuda __",
+      answer: "interagindo com a DOM para colocar componentes React na mesma",
+    },
+    {
+      question: "Usamos o npm para __",
+      answer: "gerenciar os pacotes necessários e suas dependências",
+    },
+    {
+      question: "Usamos props para __",
+      answer: "passar diferentes informações para componentes",
+    },
+    {
+      question: "Usamos estado (state) para __",
+      answer:
+        "dizer para o React quais informações quando atualizadas devem renderizar a tela novamente",
+    },
+  ]);
 
   return (
     <div className="allScreens">
@@ -24,7 +60,7 @@ export default function Body() {
             </div>
           </>
         ) : (
-          <InicialDisplay />
+          <InicialDisplay group={group} setGroup={setGroup} />
         )}
       </>
     </div>
--- src/components/InicialDisplay/InicialDisplay.js
+++ src/components/InicialDisplay/InicialDisplay.js
@@ -9,48 +9,18 @@ import Tittle from "../Tittle/Tittle";
 import selectFourCard from "../utils/selectFourCard";
 import shuffleArray from "../utils/shuffleArray";
 import Question from "../utils/Question";
+let variavel;
 
-export default function InicialDisplay() {
-  const data = [
-    {
-      numb: 1,
-      question: "O que é JSX?",
-      answer: "Uma extensão de linguagem do JavaScript",
-    },
-    {
-      question: "O React é __",
-      answer: "uma biblioteca JavaScript para construção de interfaces",
-    },
-    {
-      question: "Componentes devem iniciar com __ ",
-      answer: "letra maiúscula",
-    },
-    {
-      question: "Podemos colocar __ dentro do JSX",
-      answer: "expressões",
-    },
-    {
-      question: "O ReactDOM nos ajuda __",
-      answer: "interagindo com a DOM para colocar componentes React na mesma",
-    },
-    {
-      question: "Usamos o npm para __",
-      answer: "gerenciar os pacotes necessários e suas dependências",
-    },
-    {
-      question: "Usamos props para __",
-      answer: "passar diferentes informações para componentes",
-    },
-    {
-      question: "Usamos estado (state) para __",
-      answer:
-        "dizer para o React quais informações quando atualizadas devem renderizar a tela novamente",
-    },
-  ];
+export default function InicialDisplay({ group }) {
+  console.log(group);
+  const data = group;
 
-  shuffleArray(data);
-
-  selectFourCard(data);
+  if (variavel === undefined) {
+    shuffleArray(data);
+    selectFourCard(data);
+    variavel = "done";
+    console.log("embaralhou");
+  }
 
   const [counter, setCounter] = React.useState(0);
   const [icons, setIcons] = React.useState([]);
